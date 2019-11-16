53b3a098e2dc3984cdcccf0aceabc40c3f9fed32
abhishekshetye <abhishekshetye@ves.ac.in>
2019-11-16 16:26:25 +0530

    Initial Commit

---
+++ AnimeList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimeList
+//
+//  Created by Abhishek's iMac on 16/11/19.
+//  Copyright © 2019 Abhishek's iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
